c5b1055454ab63757254041a1d7ba96661df5ee1
NinoBarrios <ninobbarrios.bellido@gmail.com>
2024-09-05 21:47:12 -0500

    agregado pantalla para celular tablet 1.2

--- client/src/componentes/selectores.js
+++ client/src/componentes/selectores.js
@@ -3,16 +3,41 @@ import Select from 'react-select';
 
 function Selectores({ onSelectLugar, onSelectEmpresa, empresas, lugar, empresa, departamentos }) {
     // Opciones para react-select
-    const defaultLugarOption = { value: '', label: 'Departamentos' };
-    const defaultEmpresaOption = { value: '', label: 'Empresas' };
+    const defaultLugarOption = { value: '', label: 'Todos los Departamentos' };
+    const defaultEmpresaOption = { value: '', label: 'Todas las Empresas' };
 
     const lugaresOptions = [defaultLugarOption, ...departamentos.map(dep => ({ value: dep, label: dep }))];
     const empresasOptions = [defaultEmpresaOption, ...empresas.map(emp => ({ value: emp, label: emp }))];
+
+    // Estilos personalizados
+    const customStyles = {
+        control: (provided, state) => ({
+            ...provided,
+            borderColor: state.isFocused ? '#47b72f' : '#d0d0d0', // Color verde cuando está enfocado, gris cuando no lo está
+            boxShadow: state.isFocused ? '0 0 0 1px #47b72f' : null, // Sombra verde cuando está enfocado
+            '&:hover': {
+                borderColor: '#47b72f', // Borde verde cuando está en hover
+            },
+        }),
+        menu: (provided) => ({
+            ...provided,
+            zIndex: 9999,
+        }),
+        option: (provided, state) => ({
+            ...provided,
+            backgroundColor: state.isSelected ? '#47b72f' : state.isFocused ? '#e0f7e9' : provided.backgroundColor, // Fondo verde cuando está seleccionado, verde claro cuando está enfocado
+            color: state.isSelected ? 'white' : provided.color,
+            '&:hover': {
+                backgroundColor: '#e0f7e9', // Fondo verde claro cuando se pasa el mouse por encima
+            },
+        }),
+    };
 
     return (
         <div className='selectores'>
             <Select
                 className='select-lugar'
+                styles={customStyles}
                 options={lugaresOptions}
                 value={lugaresOptions.find(option => option.value === lugar) || defaultLugarOption}
                 onChange={(selectedOption) => onSelectLugar(selectedOption.value)}
@@ -21,6 +46,7 @@ function Selectores({ onSelectLugar, onSelectEmpresa, empresas, lugar, empresa, 
 
             <Select
                 className='select-lugar'
+                styles={customStyles}
                 options={empresasOptions}
                 value={empresasOptions.find(option => option.value === empresa) || defaultEmpresaOption}
                 onChange={(selectedOption) => onSelectEmpresa(selectedOption.value)}
@@ -31,4 +57,3 @@ function Selectores({ onSelectLugar, onSelectEmpresa, empresas, lugar, empresa, 
 }
 
 export default Selectores;
-
