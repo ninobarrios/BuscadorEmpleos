34ad757e09fe4c98d4a00030ca77dd40bf00d0af
NinoBarrios <ninobbarrios.bellido@gmail.com>
2024-10-16 20:10:36 -0500

    agregado 2

--- server/index.js
+++ server/index.js
@@ -8,8 +8,7 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors()); 
-
+app.use(cors());
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
