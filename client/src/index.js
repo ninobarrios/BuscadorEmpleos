4f71b2dbe96e1bddeb273bb7106bbc5396fb27a0
NinoBarrios <ninobbarrios.bellido@gmail.com>
2024-10-24 18:20:36 -0500

    agregado routes 10101010

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter> {/* Envuelve tu aplicación con BrowserRouter */}
+    <BrowserRouter> 
       <App />
     </BrowserRouter>
   </React.StrictMode>
