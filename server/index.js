916bac5676bc5fa3d169d67ce5d36dfe87297f6c
NinoBarrios <ninobbarrios.bellido@gmail.com>
2024-10-24 15:12:47 -0500

    agregado routes ++++0

--- server/index.js
+++ server/index.js
@@ -171,6 +171,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
+
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
