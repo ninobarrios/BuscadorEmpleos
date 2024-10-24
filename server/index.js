63f3efce17a2d2c3d5c952ad765866dba3a05169
NinoBarrios <ninobbarrios.bellido@gmail.com>
2024-10-24 17:12:38 -0500

    agregado routes +++333333333333

--- server/index.js
+++ server/index.js
@@ -170,6 +170,13 @@ app.get("/selecionarcarrera/:carrera", (req, res) => {
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
+app.use((req, res, next) => {
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+    res.setHeader('Surrogate-Control', 'no-store');
+    next();
+});
 
 
 app.listen(PORT, () => {
